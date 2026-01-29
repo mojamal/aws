6afc49a5c63169d76ef4474ae2ae66ddfc22b51c
Mo Jamal <mojamal@gmail.com>
2026-01-28 20:31:31 -0800

    include get_secret

---
+++ get_secret.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import boto3
+from botocorFromceptions import ClientError
+import json
+
+def get_secret(secret_name):
+    region_name = "us-east-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+    secret = get_secret_value_response['SecretString']
+    return secret
+
+# For this Example, the name of our secret in AWS Secret Manager is the Environment name
+API_KEY=json.loads(get_secret(EV_DOMAIN.lower()+'/dbt'))['dbtpersonal']
